b5b3b5bdf69dfc445a5218948c3a3598626db287
MateusRubinho <102128789+MateusRubinho@users.noreply.github.com>
2022-09-08 17:41:29 -0400

    AngularComponents

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+userName = 'Joaquim';
+
+userName2 = 'JoaquimAlemão';
+
+  title = 'AngularTeste';
+}
